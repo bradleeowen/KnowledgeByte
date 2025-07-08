89ab07b443845f97abc95ce754514c3c3fdb0e78
Owen-Kipkosgei <owen.kipkosgei@strathmore.edu>
2025-07-08 21:46:22 +0300

    Add sound effect for player hit and SFX volume slider
    Introduced a new sound effect for when the player is hit by calling SoundEffectManager.Play("PlayerHit") in PlayerHealth. Added the PlayerHit sound asset and updated the main scene with a SoundEffectManager GameObject and a UI slider for SFX volume control.

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -27,6 +27,7 @@ public class PlayerHealth : MonoBehaviour
         if (enemy)
         {
             TakeDamage(enemy.damage);
+            SoundEffectManager.Play("PlayerHit");
         }
     }
 
